0b93cef92bce3b9b4738c202c4d4ccb592596bd4
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-01-28 22:24:21 +0530

    Time: 1 ms (99.14%), Space: 42.1 MB (77.39%) - LeetHub

--- 3636-check-balanced-string/3636-check-balanced-string.java
+++ 3636-check-balanced-string/3636-check-balanced-string.java
@@ -4,8 +4,8 @@ class Solution {
       int oddc=0;
       int evenc=0;
       for(int i=0;i<num.length();i++){
-        if(i%2==0) oddc+=num.charAt(i)-'0';
-        else evenc+=num.charAt(i)-'0';
+        if(i%2==0) evenc+=num.charAt(i)-'0';
+        else oddc+=num.charAt(i)-'0';
   
       }
 
