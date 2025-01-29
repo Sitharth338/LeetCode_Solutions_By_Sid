80a33dd48180ad12d993a77306276e8bc843d900
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-01-29 22:30:06 +0530

    Time: 6 ms (12.66%), Space: 45.5 MB (42.91%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public int missingNumber(int[] nums) {
         Arrays.sort(nums);
-        int sum1=0;
+       /* int sum1=0;
         int sum2=0;
         for(int i=0;i<nums.length;i++){
           sum1 = sum1 +nums[i];
@@ -10,6 +10,15 @@ class Solution {
         for(int j=1;j<nums.length+1;j++){
             sum2 = sum2 + j;
         }
-        return sum2-sum1;
+        return sum2-sum1;   */
+        int i;
+        int j=0;
+        for( i=0;i<nums.length;i++){
+            if(nums[i] != j){
+                return i;
+            }
+            j++;
+        }
+        return i;
     }
 }
