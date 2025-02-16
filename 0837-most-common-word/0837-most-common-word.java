a9540f8413bfaed1e0d95198b03307ec29bc9025
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-02-16 22:48:14 +0530

    Time: 3 ms (99.82%), Space: 42 MB (99.91%) - LeetHub

--- 0837-most-common-word/0837-most-common-word.java
+++ 0837-most-common-word/0837-most-common-word.java
@@ -1,29 +1,59 @@
+
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        HashSet<String> banned_words = new HashSet();
-        HashMap<String, Integer> wordCount = new HashMap<>();
+        var trie = new Trie();
+        for (String w : banned)
+            trie.add(0, w, true);
+        
+        var idx = 0;
+        while (idx < paragraph.length())
+            idx = trie.add(idx, paragraph, false);
 
-        for(String banWord : banned){
-            banned_words.add(banWord);
-        }
+        if (trie.rootMaxCount == null)
+            return null;
+        
+        return paragraph.substring(trie.rootMaxCount.wordStartIdx, trie.rootMaxCount.wordEndIdx + 1).toLowerCase();
+    }
+    
+    static final class Trie {
+        Trie rootMaxCount;
 
-        String[] words = paragraph.toLowerCase().split("\\W+");
+        boolean terminal;
+        boolean banned;
+        int count;
+        int wordStartIdx;
+        int wordEndIdx;
+        Trie[] children;
+        
+        int add(int start, String text, boolean ban) {
+            if (!Character.isAlphabetic(text.charAt(start)))
+                return start + 1;
 
-        for(String word : words){
-            if(!banned_words.contains(word)){
-                wordCount.put(word, wordCount.getOrDefault(word, 0)+ 1);
+            var idx = start;
+            var current = this;
+            while (idx < text.length() && Character.isAlphabetic(text.charAt(idx))) {
+                var lc = Character.toLowerCase(text.charAt(idx++)) - 'a';
+                if (current.children == null)
+                    current.children = new Trie[26];
+                
+                var next = current.children[lc];
+                if (next == null) {
+                    current.children[lc] = new Trie();
+                    next = current.children[lc];
+                }
+                current = next;
             }
-        }
 
-        int max = 0;
-        String result = "";
+            current.terminal = true;
+            current.banned = current.banned || ban;
+            current.count++;
+            current.wordStartIdx = start;
+            current.wordEndIdx = idx - 1;
 
-        for(String word : wordCount.keySet()){
-            if(wordCount.get(word) > max){
-                max = wordCount.get(word);
-                result = word;
-            }
+            if (!current.banned && (rootMaxCount == null || rootMaxCount.count < current.count))
+                rootMaxCount = current;
+            
+            return idx;
         }
-        return result;
     }
 }
