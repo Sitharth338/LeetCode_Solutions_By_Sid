246807652218451f6f484ce4e09332deefce0598
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-02-20 22:30:40 +0530

    Time: 19 ms (16.69%), Space: 44.1 MB (16.73%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num2) {
+     //   long n1 = Long.valueOf(num1);
+     //   long n2 = Long.valueOf(num2);
+      //  long n3 = n1+n2;
+
+      BigInteger n1 = new BigInteger(num1);
+      BigInteger n2 = new BigInteger(num2);
+      BigInteger n3  = n1.add(n2);
+
+        return String.valueOf(n3);
+    }
+}
