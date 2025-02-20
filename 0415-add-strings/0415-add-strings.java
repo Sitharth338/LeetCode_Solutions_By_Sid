dd9468959e579c17c07143eabb71189a08c39b4e
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-02-20 22:31:25 +0530

    Time: 21 ms (7.76%), Space: 44 MB (22.83%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -3,7 +3,7 @@ class Solution {
     public String addStrings(String num1, String num2) {
      //   long n1 = Long.valueOf(num1);
      //   long n2 = Long.valueOf(num2);
-      //  long n3 = n1+n2;
+      //  long n3 = n1+n2;   its almost crt but not work in verylarge number;
 
       BigInteger n1 = new BigInteger(num1);
       BigInteger n2 = new BigInteger(num2);
