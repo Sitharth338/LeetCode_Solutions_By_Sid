81a2fc6bd16c6bc60649e2bbeeb834d8a01f4296
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-02-24 23:03:45 +0530

    Time: 2 ms (15.95%), Space: 41 MB (8.22%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -4,13 +4,13 @@ class Solution {
         String ans ="";
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='0'){
-                ans+='1';
+                ans+='1';   //changing 0 to 1
             }
             else{
-                ans+='0';
+                ans+='0';    //changing 1 to 0;
             }
         }
 
-        return Integer.valueOf(ans,2);
+        return Integer.parseInt(ans,2);
     }
 }
