58f32fb7f629bbb27b7d9675c183d7d7f0cab055
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-02-28 22:55:04 +0530

    Time: 2 ms (92.2%), Space: 44.7 MB (31.03%) - LeetHub

---
+++ 3533-snake-in-matrix/3533-snake-in-matrix.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int finalPositionOfSnake(int n, List<String> commands) {
+        int x=0;
+        int y=0;
+       int  len=commands.size();
+
+        for(int i=0;i<len;i++){
+            String s = commands.get(i);
+            if(s.equals("UP")){
+                x--;
+            }
+            else if(s.equals("DOWN")){
+                x++;
+            }
+            else if(s.equals("RIGHT")){
+                y++;
+            }
+            else if(s.equals("LEFT")){
+              y--;
+            }
+        }
+        return (x*n)+y;
+    }
+}
