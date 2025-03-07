67634d487fc78383c05f29a7784971e352d523c9
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-03-07 21:34:24 +0530

    Time: 0 ms (100%), Space: 42 MB (65.84%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -3,8 +3,8 @@ class Solution {
         int index = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[index] = nums[i];
-                index++;
+                nums[index++] = nums[i];
+               // index++;
             }
         }
         return index;
