3711daa89da4c4dc2babd2a8f7eba5856c4eef23
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-03-10 21:52:33 +0530

    Time: 1 ms (100%), Space: 41.9 MB (99.64%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.java
@@ -3,12 +3,14 @@ class Solution {
      int pairs=0;
     for(int i=0;i<words.length-1;i++){
      for(int j=i+1;j<words.length;j++){
-     if(words[i]==words[j] || words[i].charAt(0) == words[j].charAt(1) && words[i].charAt(1) == words[j].charAt(0)){
+        if(words[i]==words[j] || words[i].charAt(0) == words[j].charAt(1) && words[i].charAt(1) == words[j].charAt(0))
+       
+       
         pairs++;
-     }
-            }
+     
         }
+     }
 
-        return pairs;
+    return pairs;
     }
 }
