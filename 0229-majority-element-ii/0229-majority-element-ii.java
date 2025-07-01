f46d269f7aac402ccc93c5341f62d33cfdf791ed
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-07-01 08:49:21 +0530

    Time: 1620 ms (5.01%), Space: 50.2 MB (74.42%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,5 +1,18 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int more = nums.length/3;
+        Set<Integer> list = new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            int c=1;
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]) c++;
+            }
+            if(c>more){
+                list.add(nums[i]);
+            }
+        }
+          List<Integer> ls = new ArrayList<>(list);
+        return ls;
         
     }
 }
