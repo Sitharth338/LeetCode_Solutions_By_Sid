81589ec242263dd72b765f0ba764993d8197ad8f
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-07-22 22:56:02 +0530

    Time: 0 ms (100%), Space: 41.3 MB (70.52%) - LeetHub

--- 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.java
+++ 2384-root-equals-sum-of-children/2384-root-equals-sum-of-children.java
@@ -16,6 +16,6 @@
 class Solution {
     public boolean checkTree(TreeNode root) {
         int sum = root.left.val + root.right.val;
-        return sum==root.val;
+        return sum == root.val;
     }
 }
