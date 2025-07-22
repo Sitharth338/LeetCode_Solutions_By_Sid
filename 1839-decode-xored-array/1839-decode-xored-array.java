8e2237cabfc8b3f2174095d04a951afe168f5bde
Sitharthan_S <sitharthansitharthan9123@gmail.com>
2025-07-22 23:10:28 +0530

    Time: 2 ms (91.39%), Space: 45.9 MB (49.24%) - LeetHub

--- 1839-decode-xored-array/1839-decode-xored-array.java
+++ 1839-decode-xored-array/1839-decode-xored-array.java
@@ -3,7 +3,7 @@ class Solution {
         int[] ans = new int[encoded.length+1];
         ans[0] = first;
         for(int i=0;i<encoded.length;i++){
-            ans[i+1] = encoded[i] ^ ans[i];
+            ans[i+1] = encoded[i] ^ ans[i]; //xor diff =1 ...same=0;
         }
         return ans;
 
